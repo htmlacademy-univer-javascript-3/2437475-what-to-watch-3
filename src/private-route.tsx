52612dde81d7fb8e0afa7c14daa4d560cd8dc3d0
SoftOwl256 <anna.sok.03@mail.ru>
2023-10-14 16:34:02 +0500

    pls work 2.0

--- src/private-route.tsx
+++ src/private-route.tsx
@@ -11,7 +11,7 @@ export function PrivateRoute({children}: PrivateRouteProps) {
   if (!isAuthenticated) {
     return <Navigate to="/login" />;
   }
-  children = '2'; //Заглушка. Git ругался, лучше не придумала
+  children = []; //Заглушка. Git ругался, лучше не придумала
 
-  return <Navigate to="/" />; //Заглушка. Git ругался, лучше не придумала
+  return children; //Заглушка. Git ругался, лучше не придумала
 }
