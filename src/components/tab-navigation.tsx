5837c3843ea83668cfda28a05790c4ccf6d82986
SoftOwl256 <anna.sok.03@mail.ru>
2023-11-11 12:47:20 +0500

    a -> button, css

--- src/components/tab-navigation.tsx
+++ src/components/tab-navigation.tsx
@@ -8,13 +8,13 @@ export function TabsNavigation({setTab, activeTab}: PropsTabsNavigation) {
     <nav className="film-nav film-card__nav">
       <ul className="film-nav__list">
         <li className={`film-nav__item ${activeTab === 'Overview' ? 'film-nav__item--active' : ''}`}>
-          <a className="film-nav__link" onClick={() => setTab('Overview')}>Overview</a>
+          <button className="film-nav__link" onClick={() => setTab('Overview')}>Overview</button>
         </li>
         <li className={`film-nav__item ${activeTab === 'Details' ? 'film-nav__item--active' : ''}`}>
-          <a className="film-nav__link" onClick={() => setTab('Details')}>Details</a>
+          <button className="film-nav__link" onClick={() => setTab('Details')}>Details</button>
         </li>
         <li className={`film-nav__item ${activeTab === 'Reviews' ? 'film-nav__item--active' : ''}`}>
-          <a className="film-nav__link" onClick={() => setTab('Reviews')}>Reviews</a>
+          <button className="film-nav__link" onClick={() => setTab('Reviews')}>Reviews</button>
         </li>
       </ul>
     </nav>
