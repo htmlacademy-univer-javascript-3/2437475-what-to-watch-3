02ee3c39c2017809116e3335456e5ba9ca02611a
Eugene Kolegov <KollegOFF@users.noreply.github.com>
2023-11-22 22:56:25 +0500

    feat: add base for pr in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+const { GITHUB_REF = '', GITHUB_REPOSITORY = '' } = process.env;
+const isPR = /\/pull\/\d+\//.test(GITHUB_REF);
+const [,, pr] = isPR ? GITHUB_REF.split('/') : [];
+const [, repo] = isPR ? GITHUB_REPOSITORY.split('/') : [];
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: isPR ? `/${repo}/${pr}/` : '/',
   plugins: [react()],
   test: {
     globals: true,
