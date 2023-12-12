f8b3cd43171335c423e144f7d327e995bb13a817
SoftOwl256 <anna.sok.03@mail.ru>
2023-12-12 21:18:09 +0500

    mini change

--- src/api.ts
+++ src/api.ts
@@ -11,8 +11,11 @@ export function createAPI() {
 
   api.interceptors.request.use(
     (config: AxiosRequestConfig) => {
+      if (!config.headers) {
+        config.headers = {};
+      }
       const token = localStorage.getItem('token');
-      if (token && config.headers) {
+      if (token) {
         config.headers['X-Token'] = token;
       }
       return config;
@@ -22,3 +25,13 @@ export function createAPI() {
   return api;
 }
 
+/**
+ * Argument of type '(config: AxiosRequestConfig) => AxiosRequestConfig<any>' is not assignable to parameter of type '(value: InternalAxiosRequestConfig<any>) => InternalAxiosRequestConfig<any> | Promise<InternalAxiosRequestConfig<any>>'.
+  Type 'AxiosRequestConfig<any>' is not assignable to type 'InternalAxiosRequestConfig<any> | Promise<InternalAxiosRequestConfig<any>>'.
+    Type 'AxiosRequestConfig<any>' is not assignable to type 'InternalAxiosRequestConfig<any>'.
+      Types of property 'headers' are incompatible.
+        Type 'AxiosHeaders | (Partial<RawAxiosHeaders & { Accept: AxiosHeaderValue; "Content-Length": AxiosHeaderValue; "User-Agent": AxiosHeaderValue; "Content-Encoding": AxiosHeaderValue; Authorization: AxiosHeaderValue; } & { ...; }> & Partial<...>) | undefined' is not assignable to type 'AxiosRequestHeaders'.
+          Type 'undefined' is not assignable to type 'AxiosRequestHeaders'.
+            Type 'undefined' is not assignable to type 'Partial<RawAxiosHeaders & { Accept: AxiosHeaderValue; "Content-Length": AxiosHeaderValue; "User-Agent": AxiosHeaderValue; "Content-Encoding": AxiosHeaderValue; Authorization: AxiosHeaderValue; } & { ...; }>'.ts(2345)
+
+ */
