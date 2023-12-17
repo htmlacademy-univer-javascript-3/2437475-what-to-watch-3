497f09fd9f601fc2b7cd8cb239649da13791cb1b
SoftOwl256 <anna.sok.03@mail.ru>
2023-12-17 14:11:41 +0500

    reducer.test

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {App} from './components/app';
+import { App } from './components/app';
 import { Detail } from './mocks/details';
 import store from './store';
 import { Provider } from 'react-redux';
